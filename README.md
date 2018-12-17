5a594cb630bf0b6fde0e066b31d9dbbee594868f
Jason Wang <wang.jason.010@gmail.com>
2018-12-16 19:54:00 -0800

    yes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # MEAN-Group-Project
+
+Priority Tasks:
+Home Component - Create a nice welcome interface for signup/thread creation
+User Component - Create unique users, input to database, display current user on navbar
+Thread Component - Be able to create thread rooms and display thread list
+Thread Room Component - Display thread, comments, chat, and user list (to the side)
+
+Optional Tasks:
+Add authentication for users, threads, and comments
+Optimize and sanitize database input
+Ability to search/filter/sort threads
+Add user profile functionality
+Spiffy up the styling
+
+REACH Tasks:
+Integrate voice-comm functionality
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const http = require("http");
+const server = http.Server(app);
+
+const socketIO = require("socket.io");
+const io = socketIO(server);
+
+const port = process.env.PORT || 3000;
+
+io.on("connection", socket => {
+  console.log("user connected");
+
+  socket.on("new-message", message => {
+    console.log(message);
+    io.emit("emit-new-message", message);
+  });
+});
+
+app.use(express.static(__dirname + "/public/dist/public"));
+app.use(bodyParser.json());
+
+app.all("*", (req, res, next) => {
+  res.sendFile(path.resolve("./public/dist/public/index.html"));
+});
+
+server.listen(port, () => {
+  console.log(`started on port: ${port}`);
+});
