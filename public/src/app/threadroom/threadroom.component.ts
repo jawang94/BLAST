c3dabed2e95cbc5c75b05b3e2b168031193750f1
Masa Hanatate <mhanatate@gmail.com>
2018-12-17 12:09:53 -0800

    navbar first commit

---
+++ public/src/app/threadroom/threadroom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-threadroom',
+  templateUrl: './threadroom.component.html',
+  styleUrls: ['./threadroom.component.css']
+})
+export class ThreadroomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
