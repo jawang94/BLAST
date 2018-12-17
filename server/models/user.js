64f33bc46b69c9a99e9728634f12f6eb55aefff4
Jason Lu <jasonlu121@gmail.com>
2018-12-17 11:27:41 -0800

    added in base user, thread, and comment schemas

--- server/models/user.js
+++ server/models/user.js
@@ -1,27 +1,38 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 
-var UserSchema = new mongoose.schema(
+const UserSchema = new mongoose.schema(
 {
     username: { 
         type: String, 
         Required: true, 
-        minlength: [3, "Username needs to be longer than 3 characters"],
-        maxlength: [16, "Username cannot be more than 16 characters long"]
+        minlength: [3, "Username must be longer than 3 characters"],
+        maxlength: [16, "Username cannot be more than 16 characters long"],
+        
     }
-}
-)
+})
 
 
 const ThreadSchema = new mongoose.Schema({
     title: {
         type:String,
         required: true,
-        minlength:[3,"Please enter a  name"]
+        minlength:[3,"Title must be longer than 3 characters "]
         },
     Catergory: {
         type:String,
         required: true,
-        minlength:[1, "URL PLZ!!!"]},
-    users: [UserSchema]
+        },
+    users: [UserSchema],
+    comments: [CommentSchema]
+})
+
+
+const CommentSchema = new mongoose.Schem({
+    Content: {
+        type: String,
+        required: true,
+        user: [UserSchema, maxlength:[1]],
+        minlength:[1,"Comments cannot be empty"]
+    }
 })
