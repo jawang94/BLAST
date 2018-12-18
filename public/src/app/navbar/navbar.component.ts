2e53817dd8286153c5e3ea36f1a4bad371f2948f
Masa Hanatate <mhanatate@gmail.com>
2018-12-18 13:35:26 -0800

    merging for fixes

--- public/src/app/navbar/navbar.component.ts
+++ public/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { HttpService } from "../services/http.service";
 import { Router } from "@angular/router";
 
@@ -8,10 +8,11 @@ import { Router } from "@angular/router";
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  
+  @Input() user: any;
   constructor(private _router: Router, private _httpService: HttpService) { }
-
+  
   ngOnInit() {
   }
-
+  
 }
