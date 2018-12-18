f92d0ab8c5798fece58391d3776867e384717f45
Jason Wang <wang.jason.010@gmail.com>
2018-12-17 17:18:13 -0800

    1. Added jQuery to hide and show appropriate content 2. Added user creation and message creation with associated user 3. Sanitized inputs

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,32 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ChatService } from "./services/chat.service";
+import * as $ from "jquery";
+import * as io from "socket.io-client";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  message: string;
-  messages: string[] = [];
+export class AppComponent implements OnInit {
+  message: any;
+  messages: any[] = [];
   user: any;
   users: any[] = [];
 
   constructor(private chatService: ChatService) {}
 
   public sendMessage() {
+    this.message.user = this.user;
     this.chatService.sendMessage(this.message);
-    this.message = "";
+    this.message = { content: "", user: "" };
   }
 
   public onSubmit() {
     this.chatService.register(this.user);
-    this.user = "";
   }
 
   ngOnInit() {
-    this.user = { name: "", id: "" };
-
     this.chatService.getMessages().subscribe((message: string) => {
       console.log(message);
       this.messages.push(message);
@@ -34,6 +34,22 @@ export class AppComponent {
     this.chatService.getUsers().subscribe((user: any) => {
       console.log(user);
       this.users.push(user);
+    });
+
+    this.user = { name: "", id: "" };
+    this.message = { content: "", user: "" };
+
+    $(document).ready(function() {
+      var socket = io.connect("http://localhost:3000");
+
+      socket.on("emit-new-user", function(data) {
+        console.log("got that user");
+        $("#container").fadeOut(1000);
+        $("#conversation_board").fadeIn(1000);
+        $("#messageForm").fadeIn(1000);
+        $("#title").fadeIn(1000);
+        $("#user_list").fadeIn(1000);
+      });
     });
   }
 }
